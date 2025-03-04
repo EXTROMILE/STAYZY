8fba694f74057561b6a69e0f25cd22d7c581084e
EXTROMILE <sinhaanal441@gmail.com>
2025-03-04 13:22:47 +0530

    third commit

--- controller/listings.js
+++ controller/listings.js
@@ -1,7 +1,7 @@
 const Listing = require("../models/listing.js");
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken = process.env.MAP_TOKEN;
-const geoCodingClient = mbxGeocoding({ accessToken: mapToken });
+const mapToken =  process.env.MAP_TOKEN ;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 // * Index Route
 module.exports.index = async (req, res) => {
@@ -31,21 +31,20 @@ module.exports.showListings = async (req, res) => {
 // * Create Route
 module.exports.createListings = async (req, res, next) => {
 
-  let response = await geoCodingClient
-  .forwardGeocode({
+  let response = await geocodingClient.forwardGeocode({
     query: req.body.listing.location,
     limit: 1,
   })
-    .send();
-  
+    .send()
+
   let url = req.file.path;
   let filename = req.file.filename;
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename};
   newListing.geometry = response.body.features[0].geometry;
-  await newListing.save();
-  console.log(newListing);
+  let savedListing = await newListing.save();
+  console.log(savedListing);
   req.flash("success", "Listing added!");
   res.redirect("/listings");
 };
--- public/js/map.js
+++ public/js/map.js
@@ -1,18 +1,19 @@
+// let mapToken = mapToken;
+// console.log(mapToken);
 mapboxgl.accessToken = mapToken;
-
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12",
   center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
   zoom: 9, // starting zoom
 });
 
 console.log(coordinates);
-const marker = new mapboxgl.Marker({ color: "red" })
-  .setLngLat(coordinates)
-  .setPopup(
-    new mapboxgl.Popup({ offset: 25 }).setHTML(
-      `<h5>${listing.title}</h5><p>Exact location will be provided after booking</p>`
-    )
-  )
-  .addTo(map);
+
+ // Create a default Marker and add it to the map.
+ const marker = new mapboxgl.Marker({ color: 'red'})
+ .setLngLat(coordinates)
+ .setPopup(new mapboxgl.Popup({offset: 25,})
+ .setHTML(
+  `<p>Exact location will be provided after booking</p>`
+))
+ .addTo(map);
